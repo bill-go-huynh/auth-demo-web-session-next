78fa0238e5509df45f924429de4321f5c5eda38d
Bill Huynh <bill.huynh.gos@gmail.com>
2025-11-24 14:07:04 +0700

    Refactor AuthInitializer component: removed unnecessary user and loading state checks, simplified user fetching logic on mount.

--- src/components/StoreProvider.tsx
+++ src/components/StoreProvider.tsx
@@ -5,27 +5,19 @@ import { Provider } from 'react-redux';
 
 import { store } from '@/store';
 import { fetchMe } from '@/store/features/auth';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { useAppDispatch } from '@/store/hooks';
 
 function AuthInitializer({ children }: { children: ReactNode }) {
   const dispatch = useAppDispatch();
-  const { user, loading } = useAppSelector((state) => state.auth);
   const hasTriedFetchRef = useRef(false);
 
   useEffect(() => {
-    // Fetch user info on mount if not loading and no user
-    if (!loading && !user && !hasTriedFetchRef.current) {
+    if (!hasTriedFetchRef.current) {
       hasTriedFetchRef.current = true;
-      dispatch(fetchMe()).catch(() => {
-        // Ignore errors, they're handled by the slice
-      });
+      dispatch(fetchMe()).catch(() => {});
     }
-
-    // Reset hasTriedFetch if user is removed
-    if (!user) {
-      hasTriedFetchRef.current = false;
-    }
-  }, [user, loading, dispatch]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <>{children}</>;
 }
